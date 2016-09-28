5e87b0f53ef5172d4d498cbed4fc9e3142145701
Максим <mderemin@gmail.com>
2016-09-28 22:37:47 +0400

    Add files via upload

--- data.c
+++ data.c
@@ -46,44 +46,44 @@ void Data_print(Element **elems, size_t i, size_t j)
 {
 	for (size_t ix = 0; ix < i; ix++) {
 		for (size_t jy = 0; jy < j; jy++) {
-			printf("%llu -- %s\n", Data_key_return(elems, ix, jy), elems[ix][jy].string);
+			printf("%llu -- %s\n",elems[ix][jy].key, elems[ix][jy].string);
 		}
 	}
 }
+
+// 1 8 4 4 6 7 4 4 0 7 3 7 0 9 5 5 1 6 1 5
 
 void Data_bucket_sort(Element **elems, size_t i, size_t j)
 {
-	if (i <= 2) {
-		return;
-	}
-	unsigned long long max = 0;
+	size_t size_of_array = i * j;
+	Element **tmp = elems;
 	unsigned long long min = ULLONG_MAX;
-
-	for (size_t ix = 0; ix < i; ix++) {
-		for (size_t jy = 0; jy < j; jy++) {
-			if (max < elems[ix][jy].key) {
-				max = elems[ix][jy].key;
-			}
+	unsigned long long max = 0;
+	for (size_t k = 0; k < i; k++) {
+		if (min < tmp[k][0].key) {
+			min = tmp[k][0].key;
+		}
+		if (max > tmp[k][0].key) {
+			max = tmp[k][0].key;
 		}
 	}
-	for (size_t ix = 0; ix < i; ix++) {
-		for (size_t jy = 0; jy  j; jy++) {
-			if (min > elems[ix][jy].key) {
-				min = elems[ix][jy].key;
-			}
+	size_t idx = 1;
+	Element **buckets = Data_create(size_of_array + 1, 1);
+	for (size_t k = 1; k <= size_of_array; k++) {
+		Data_insert(buckets, 10 * size_of_array, (tmp[k][0].key / ULLONG_MAX) * size_of_array, idx++, tmp[k][0].key, tmp[k][0].string);
+	}
+	for(size_t k = 1; k <= idx; k++) {
+		Data_bucket_sort(buckets + k - 1, size_of_array + 1, 1);
+	}
+	for (size_t k = 1; k <= size_of_array + 1; k++) {
+		for (size_t n = 1; n <= idx; n++) {
+			Data_insert(elems, i * j, k, n, buckets[k][n].key, buckets[k][n].string);
 		}
 	}
-	Element **bucket = Data_create(,);
-	unsigned long long idx = 0;
-	for (size_t ix = 0; ix < i; ix++) {
-		for (size_t jy; jy < j; jy++) {
-			index = (elems[ix][jy].key)
-		}
-	}
-	Data_destroy(bucket, ,)
-
-
+	Data_destroy(tmp, i, j);
+	Data_destroy(buckets, size_of_array + 1, idx);
 }
+
 void Data_destroy(Element **elems, size_t i, size_t j)
 {
 	for (size_t ix = 0; ix < i; ix++) {
